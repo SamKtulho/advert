6d12fec51a32496f456e1ccc9dfdb17ef02a7d1c
German <sam@coccoc.com>
2017-03-28 19:13:31 +0700

    change aff id

--- app/Services/Affiliate.php
+++ app/Services/Affiliate.php
@@ -11,10 +11,26 @@ namespace App\Services;
 class Affiliate {
 
     const DEFAULT_LAZADA_URI = 'http://ho.lazada.vn';
-    const DEFAULT_LAZADA_CODE = 'SHNXqB';
+    const DEFAULT_LAZADA_CODE = 'SHNXhl';
+
+    private static $requiredParams = [
+        'offer_id' => '{offer_id}',
+        'affiliate_id' => '{affiliate_id}',
+        'offer_name' => '{offer_name}_{offer_file_id}',
+        'affiliate_name' => '{affiliate_name}',
+        'transaction_id' => '{transaction_id}'
+    ];
+
+    /**
+     * @return array
+     */
+    public static function getRequiredParams()
+    {
+        return self::$requiredParams;
+    }
 
     public static function getDefaultRedirectUrl()
     {
-        return self::DEFAULT_LAZADA_URI . '/' . self::DEFAULT_LAZADA_CODE . '?url=' . urlencode('http://lazada.vn');
+        return self::DEFAULT_LAZADA_URI . '/' . self::DEFAULT_LAZADA_CODE . '?url=' . urlencode('http://lazada.vn?' . http_build_query(self::$requiredParams));
     }
 }
